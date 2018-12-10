a366be81759d285c06729f4ab41c7d9bad82532b
=Gil Méndez <gmendez@gbnetworks.mx>
2018-12-10 14:35:45 -0600

    :pencil: new directory: js files

---
+++ public/js/app.js
@@ -0,0 +1,48 @@
+new Vue({
+    el: '#app', 
+    data(){
+        return {
+            state: '',
+            city: '',
+            states: [],
+            cities: []
+        }
+    },
+    mounted(){
+        this.getStates();
+        this.getCities();
+    },
+    methods: {
+        getStates(){
+            axios.get('/api/states')
+            .then(response => {
+                this.states = response.data.states;
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        },
+        getCities(){
+            axios.get('/api/states/cities')
+            .then(response => {
+                this.cities = response.data.cities;
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    },
+    computed: {
+        invalidForm(){
+            return this.state == '' || this.city == ''
+        },
+        filteredCities(){
+            return this.cities.filter( city => city.state_id == this.state)
+        }
+    },
+    watch: {
+        state(){
+            this.city = ''
+        }
+    }
+});
